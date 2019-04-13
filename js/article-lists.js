4657aa7a26e0198abb81069cf67754b523e7478f
吴小迪 <1336791007@qq.com>
2019-04-13 11:21:14 +0800

    test1

---
+++ js/article-lists.js
@@ -0,0 +1,5 @@
+let h2 = document.getElementsByTagName('h2')[0];
+console.log(h2)
+h2.onclick=()=>{
+    alert('222')
+}
---
+++ js/index.js
@@ -0,0 +1,5 @@
+var h1 = document.getElementsByTagName('h1')[0];
+console.log(h1)
+h1.onclick=()=>{
+    alert("111")
+}
