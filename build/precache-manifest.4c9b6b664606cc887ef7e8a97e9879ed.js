1312cd7080e2a95b468cace5af4930e6535d27f5
wuxiaodi <1336791007@qq.com>
2019-11-24 22:13:27 +0800

    test

---
+++ build/precache-manifest.4c9b6b664606cc887ef7e8a97e9879ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52d4adc840bfb84472e76d34b81a4cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be9a6ba890e3b0d05768",
+    "url": "/static/css/main.b63e79f0.chunk.css"
+  },
+  {
+    "revision": "587bd526735de9dc239b",
+    "url": "/static/js/2.5792cc28.chunk.js"
+  },
+  {
+    "revision": "be9a6ba890e3b0d05768",
+    "url": "/static/js/main.276b7af8.chunk.js"
+  },
+  {
+    "revision": "bd8fa7d7a029998c5380",
+    "url": "/static/js/runtime-main.d972c872.js"
+  }
+]);
