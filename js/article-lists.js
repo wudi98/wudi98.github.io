931fee30962cf2b4ad18a64473f7ce2798b39d9b
wuxiaodi <1336791007@qq.com>
2019-11-24 22:23:14 +0800

    哈哈

--- js/article-lists.js
+++
@@ -1,5 +0,0 @@
-let h2 = document.getElementsByTagName('h2')[0];
-console.log(h2)
-h2.onclick=()=>{
-    alert('222')
-}
--- js/index.js
+++
@@ -1,5 +0,0 @@
-var h1 = document.getElementsByTagName('h1')[0];
-console.log(h1)
-h1.onclick=()=>{
-    alert("111")
-}
